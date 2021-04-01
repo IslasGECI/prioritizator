9c9204c535cc252c16e7548bcf8ca1028e9860cc
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-01 16:10:36 -0700

    🛑 Add failing test

--- tests/test_Task.py
+++ tests/test_Task.py
@@ -1,8 +1,14 @@
 from prioritizator import Task
 
 
-def test_Task():
+def test_rating():
     task = Task()
     obtained_rating = round(task.rating)
     expected_rating = 1500
     assert expected_rating == obtained_rating
+
+def test_match():
+    task_1 = Task()
+    task_2 = Task()
+    task_1.match(task_2,1)
+    assert task_1.rating > task_2.rating
