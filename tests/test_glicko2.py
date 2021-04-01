ef0c7a2da5be8b5a513eeb122e9e36a043007b7f
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-01 15:59:11 -0700

    ✅ Rename variable

--- tests/test_glicko2.py
+++ tests/test_glicko2.py
@@ -2,5 +2,5 @@ import glicko2
 
 
 def test_glicko2():
-    player1 = glicko2.Player()
-    assert isinstance(player1, glicko2.glicko2.Player)
+    player = glicko2.Player()
+    assert isinstance(player, glicko2.glicko2.Player)
