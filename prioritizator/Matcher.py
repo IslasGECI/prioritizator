d393507a1444fd9e665265aeccc7ce25b3609310
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-03 13:53:43 -0700

    🥅 Implement match for new tasks

--- prioritizator/Matcher.py
+++ prioritizator/Matcher.py
@@ -6,8 +6,14 @@ class Matcher:
         self.data_path = data_path
 
     def match(self, white_id, black_id, outcome):
-        white_task = Task(id=white_id).load_from_csv(self.data_path)
-        black_task = Task(id=black_id).load_from_csv(self.data_path)
+        try:
+            white_task = Task(id=white_id).load_from_csv(self.data_path)
+        except LookupError:
+            white_task = Task(id=white_id)
+        try:
+            black_task = Task(id=black_id).load_from_csv(self.data_path)
+        except LookupError:
+            black_task = Task(id=black_id)
         white_task.match(black_task, outcome)
         white_task.save_to_csv(self.data_path)
         black_task.save_to_csv(self.data_path)
