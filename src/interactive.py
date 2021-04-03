316a66154d8e7d5b3183afa21f8c947644c6f361
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-03 16:03:16 -0700

    🪲 Parse inputs as numbers

--- src/interactive.py
+++ src/interactive.py
@@ -4,12 +4,9 @@ from random import randint
 DATA_PATH = "data/task_list.csv"
 PAGES = 5
 TASKS_PER_PAGE = 25
-WHITE_WINS = 1
-DRAW = 0.5
-WHITE_LOSES = 0
 
-white_id = input(f"White: Page: {randint(1,PAGES)}, Issue: {randint(1,TASKS_PER_PAGE)}\nID?> ")
-black_id = input(f"Black: Page: {randint(1,PAGES)}, Issue: {randint(1,TASKS_PER_PAGE)}\nID?> ")
-outcome = input(f"White wins = 1, Draw = 0.5, Black wins = 0\nOutcome?> ")
+white_id = int(input(f"White: Page: {randint(1,PAGES)}, Issue: {randint(1,TASKS_PER_PAGE)}\nID?> "))
+black_id = int(input(f"Black: Page: {randint(1,PAGES)}, Issue: {randint(1,TASKS_PER_PAGE)}\nID?> "))
+outcome = float(input(f"White wins = 1, Draw = 0.5, Black wins = 0\nOutcome?> "))
 matcher = Matcher(DATA_PATH)
-matcher.match(white_id=white_id, black_id=black_id, outcome=WHITE_LOSES)
+matcher.match(white_id=white_id, black_id=black_id, outcome=outcome)
