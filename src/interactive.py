818993597af1642ce5ae609ec3879d517535adf0
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-04 11:15:15 -0700

    🪲 Remove f-string

--- src/interactive.py
+++ src/interactive.py
@@ -7,6 +7,6 @@ TASKS_PER_PAGE = 25
 
 white_id = int(input(f"White: Page: {randint(1,PAGES)}, Issue: {randint(1,TASKS_PER_PAGE)}\nID?> "))
 black_id = int(input(f"Black: Page: {randint(1,PAGES)}, Issue: {randint(1,TASKS_PER_PAGE)}\nID?> "))
-outcome = float(input(f"White wins = 1, Draw = 0.5, Black wins = 0\nOutcome?> "))
+outcome = float(input("White wins = 1, Draw = 0.5, Black wins = 0\nOutcome?> "))
 matcher = Matcher(DATA_PATH)
 matcher.match(white_id=white_id, black_id=black_id, outcome=outcome)
