651fd6e24b118a6ad383e2f14d6fa9e7cc508644
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-04 14:33:51 -0700

    Add failing test for method update_rating_on_github

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -4,10 +4,18 @@ from prioritizator import Task
 def test_set_and_get_title():
     task = Task(id=1)
     expected_title = "Hola Mundo"
-    obtained_title = task._set_title(expected_title)
+    task._set_title(expected_title)
     obtained_title = task._get_title()
     assert expected_title == obtained_title
     expected_title = "Figura circular Objetivos Desarrollo Sostenible ONU"
-    obtained_title = task._set_title(expected_title)
+    task._set_title(expected_title)
     obtained_title = task._get_title()
+    assert expected_title == obtained_title
+
+
+def test_update_rating_on_github():
+    task = Task(id=1)
+    task.update_rating_on_github()
+    obtained_title = task._get_title()
+    expected_title = "[1500] Figura circular Objetivos Desarrollo Sostenible ONU"
     assert expected_title == obtained_title
